fcfd14deffaca75518c8fe3f528f0ded86b941c1
poobanaaselvam <pooanaaselvam@gmail.com>
2020-02-06 19:10:10 -0600

    SHOOTERHOOD part 1

--- src/main/java/org/usfirst/frc/team3042/robot/RobotMap.java
+++ src/main/java/org/usfirst/frc/team3042/robot/RobotMap.java
@@ -49,7 +49,9 @@ public class RobotMap {
 	
 	/** PCM channels **********************************************************/
 
-	public static final int INTAKE_DEPLOY_SOLENOID = 			(IS_PBOT) 	? 9 : 2;	
+	public static final int INTAKE_DEPLOY_SOLENOID =			(IS_PBOT) 	? 9 : 2;
+	public static final int INTAKE_DEPLOY_SOLENOID2 =			(IS_PBOT) 	? 9 : 2;
+	public static final int SHOOTER_HOOD_SOLENID =			(IS_PBOT) 	? 9 : 2;	
 
 	/** SPI ports *************************************************************/
 	//note that the Gyroscope uses the myRIO Expansion Port (MXP) and is defined in the SPI class (edu.wpi.first.wpilibj.SPI)
--- src/main/java/org/usfirst/frc/team3042/robot/subsystems/IntakeDeploy.java
+++ src/main/java/org/usfirst/frc/team3042/robot/subsystems/IntakeDeploy.java
@@ -14,14 +14,21 @@ public class IntakeDeploy extends Subsystem {
 	/** Configuration Constants ***********************************************/
 	private static final Log.Level LOG_LEVEL = RobotMap.LOG_INTAKE_DEPLOY;
 	private static final int ID = RobotMap.INTAKE_DEPLOY_SOLENOID;
+	private static final int ID2 = RobotMap.INTAKE_DEPLOY_SOLENOID2;
 
 	/** Instance Variables ****************************************************/
 	  Log log = new Log(LOG_LEVEL, SendableRegistry.getName(this));
 	  Solenoid intakeDeploySolenoid = new Solenoid(ID);
+	  Solenoid intakeDeploySolenoid2 = new Solenoid(ID2);
 
 	/** Intake Deploy ******************************************************/
 	public IntakeDeploy() {
     	log.add("Constructor", LOG_LEVEL);
+	}
+
+	public void activate(){
+		intakeDeploySolenoid.set(true);
+		intakeDeploySolenoid2.set(true);
 	}
 	
 	/** initDefaultCommand ****************************************************
@@ -29,9 +36,5 @@ public class IntakeDeploy extends Subsystem {
 	 */
 	public void initDefaultCommand() {
 		setDefaultCommand(null);
-	}
-	/** IntakeDeploy ****************************************************/
-	public void activate(){
-		intakeDeploySolenoid.set(true);
 	}
 }
